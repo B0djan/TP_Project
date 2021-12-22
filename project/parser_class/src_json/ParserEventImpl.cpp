6f4c4bf112705b8e81c51c9ac70778212d531482
Bodja_n <bs@mt11.su>
2021-12-22 19:57:24 +0300

    Check_Compilation

---
+++ project/parser_class/src_json/ParserEventImpl.cpp
@@ -0,0 +1,46 @@
+#include <ParserEventImpl.hpp>
+
+ParserObject ParserEventImpl::StrToObject(const std::string& parser_str) const {
+    
+    // nlohmann::json j = nlohmann::json::parse(parser_str);
+
+    // nlohmann::json::iterator it = j.begin();
+
+    // nlohmann::json value = j[it.key()];
+
+    // std::set<event_t> events;
+
+    // for (auto& element : value[""])
+
+    //     if(j.contains("user_id"))
+    //         contacts.user_id = value["user_id"];
+
+    //     if(j.contains("contacts"))
+    //     {
+    //         for (auto& element : value["contacts"])
+    //         contacts.contacts.insert(element.dump());
+    //     }
+
+    // ParserObject res;
+
+    // res.events = events;
+
+    // return res;
+}
+
+std::string ParserEventImpl::ObjectToStr(const ParserObject& other) const {
+
+    contacts_t contacts = other.contacts;
+
+    nlohmann::json value;
+
+    nlohmann::json j;
+
+    j["friends"] = value;
+
+    std::string res = j.dump();
+
+    // {"registration":{"user_id":"value"}}
+
+    return res;
+}
