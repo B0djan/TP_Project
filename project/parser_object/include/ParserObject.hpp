ed17ac68a0df3c5578f79989b7fa3c05bfa167cb
Bodja_n <bs@mt11.su>
2021-12-24 02:11:31 +0300

    Add parser_object/ParserObject.cpp + CMakeList.txt text

--- project/parser_object/include/ParserObject.hpp
+++ project/parser_object/include/ParserObject.hpp
@@ -64,17 +64,11 @@ typedef struct {
     std::set<std::string> contacts;
 } contacts_t;
 
-    bool operator< (const event_t& lhs, const event_t& rhs) {
-    return lhs.time_begin < rhs.time_begin;
-    }
+    bool operator< (const event_t& lhs, const event_t& rhs);
 
-    bool operator< (const meetup_t& lhs, const meetup_t& rhs) {
-    return lhs.time_begin < rhs.time_begin;
-    }
+    bool operator< (const meetup_t& lhs, const meetup_t& rhs);
 
-    bool operator< (const group_t& lhs, const group_t& rhs) {
-    return lhs.title < rhs.title;
-    }
+    bool operator< (const group_t& lhs, const group_t& rhs);
 
 class ParserObject {
 public:
---
+++ project/parser_object/src/ParserObject .cpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+#include <set>
+
+#include "json.hpp"
+
+#include "ParserObject.hpp"
+
+    bool operator< (const event_t& lhs, const event_t& rhs) {
+    return lhs.time_begin < rhs.time_begin;
+    }
+
+    bool operator< (const meetup_t& lhs, const meetup_t& rhs) {
+    return lhs.time_begin < rhs.time_begin;
+    }
+
+    bool operator< (const group_t& lhs, const group_t& rhs) {
+    return lhs.title < rhs.title;
+    }
