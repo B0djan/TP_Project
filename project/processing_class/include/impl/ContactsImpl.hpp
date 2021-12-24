198548019a792c6db23256588073528f6e7dbe70
Bodja_n <bs@mt11.su>
2021-12-24 03:33:36 +0300

    database_connector/SQL/pool

--- project/processing_class/include/impl/ContactsImpl.hpp
+++ project/processing_class/include/impl/ContactsImpl.hpp
@@ -3,11 +3,15 @@
 #include <Handler.hpp>
 
 class AddUserContactsImpl : public Handler {
+    std::string AddFriend(contacts_t& c);
 public:
     ParserObject process(const ParserObject& request_body) override;
 };
 
 class RmUserContactsImpl : public Handler {
+    std::string DeleteFriend(contacts_t& c);
 public:
     ParserObject process(const ParserObject& request_body) override;
+private:
+  
 };
--- project/processing_class/src/ContactsImpl.cpp
+++ project/processing_class/src/ContactsImpl.cpp
@@ -1,13 +1,33 @@
 #include <ContactsImpl.hpp>
 
 ParserObject AddUserContactsImpl::process(const ParserObject& request_body) {
+
+    contacts_t contacts = request_body.contacts;
+
+    std::string code = AddFriend(contacts);
+
     ParserObject response_body;
     return response_body;
 }
 
 ParserObject RmUserContactsImpl::process(const ParserObject& request_body) {
+
+    contacts_t contacts = request_body.contacts;
+
+    std::string code = DeleteFriend(contacts);
+
     ParserObject response_body;
     return response_body;
 }
+
+std::string AddUserContactsImpl::AddFriend(contacts_t& c) {
+    std::string code;
+    return code;
+};
+
+std::string RmUserContactsImpl::DeleteFriend(contacts_t& c) {
+    std::string code;
+    return code;
+};
 
 
