efcee00bae267c160e3c5af6c3e84d74a7f89bed
Andeo1812 <alexanforall@gmail.com>
2021-12-27 12:56:04 +0300

    + syncro for add rm friend

--- project/processing_class/src/ContactsImpl.cpp
+++ project/processing_class/src/ContactsImpl.cpp
@@ -18,7 +18,14 @@ ParserObject AddUserContactsImpl::process(const ParserObject& request_body) {
 
     code = DatabaseConnector::Contact::Add(request_body.contacts.user_id, user_id);
     if (code != 0) {
-        response_body.error = "Error add contact";
+        response_body.error = "Error add first contact";
+
+        return response_body;
+    }
+
+    code = DatabaseConnector::Contact::Add(user_id, request_body.contacts.user_id);
+    if (code != 0) {
+        response_body.error = "Error add second contact";
 
         return response_body;
     }
@@ -33,13 +40,29 @@ ParserObject RmUserContactsImpl::process(const ParserObject& request_body) {
 
     ParserObject response_body;
 
-    code = DatabaseConnector::Contact::Delete(request_body.contacts.user_id,
-                                              DatabaseConnector::GetID::User(*it_m));
-    if (code != 0) {
-        response_body.error = "Error delete contact";
+    char* check_user_for_del = DatabaseConnector::GetID::User(*it_m);
+    if (check_user_for_del == NULL) {
+        response_body.error = "Error get user id";
 
         return response_body;
     }
+
+    std::string user_id_1 = check_user_for_del;
+
+    code = DatabaseConnector::Contact::Delete(request_body.contacts.user_id, user_id_1);
+    if (code != 0) {
+        response_body.error = "Error delete for first contact";
+
+        return response_body;
+    }
+
+    code = DatabaseConnector::Contact::Delete(user_id_1, request_body.contacts.user_id);
+    if (code != 0) {
+        response_body.error = "Error delete for second contact";
+
+        return response_body;
+    }
+
 
     return response_body;
 }
